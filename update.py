b35a0bf4b28e79c7d4169c2f62e75350eb6c0f78
Arindam Das Modak <arindam.modak03@gmail.com>
2018-11-08 15:32:38 +0530

    Update update.py

--- update.py
+++ update.py
@@ -3,11 +3,28 @@ import subprocess
 
 #subprocess.call(['chmod', '0777', '/etc/squid/squid.conf'])
 def update_port(port_num):
-	file = open('/etc/squid/squid.conf','w') 
+	file = open('/etc/squid/squid.conf','a') 
 	file.write("\n"+"acl Safe_ports port "+str(port_num))
 	file.write("\n"+"http_access deny !Safe_ports")
 	file.close()
 	subprocess.call('sudo systemctl reload squid.service',shell=True)
+
+
+def add_mac(username,mac):
+	file = open('/etc/squid/config_files/'+username+'/'+username+'_mac.lst','a') 
+	file.write("\n"+str(mac))
+	file.close()
+	subprocess.call('sudo systemctl reload squid.service',shell=True)
+
+
+def add_website(username,website):
+	file = open('/etc/squid/config_files/'+username+'/'+username+'_website.lst','a') 
+	file.write("\n"+str(website))
+	file.close()
+	subprocess.call('sudo systemctl reload squid.service',shell=True)
+
+
+
 
 
 # squid.conf to temp.txt
